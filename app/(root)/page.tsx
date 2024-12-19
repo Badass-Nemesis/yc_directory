b3faaef4b8421907931600027cb9d160db765527
Badass-Nemesis <harshitanant29@gmail.com>
2024-12-19 23:24:38 +0530

    .

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -9,7 +9,7 @@ export default async function Home({ searchParams }: { searchParams: Promise<{ q
       <section className="pink_container">
         <h1 className="heading">Pitch Your Startup, <br /> Connect With Entrepreneurs</h1>
         <p className="sub-heading !max-w-3xl">
-          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.
         </p>
 
         <SearchForm query={query} />

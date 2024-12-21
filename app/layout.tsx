baf22365a37a38118c9a1a4c522fdf4f804b6e0f
Badass-Nemesis <harshitanant29@gmail.com>
2024-12-21 19:20:38 +0530

    added a sanity markdown plugin and had to put some easymde css import in layout to make it look good in future

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import localFont from 'next/font/local';
 import "./globals.css";
+import 'easymde/dist/easymde.min.css'
 
 // Define the local font configuration
 const workSans = localFont({

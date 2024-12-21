2747f1ebbacbad55557d1c65bb0ea42a550c69df
Badass-Nemesis <harshitanant29@gmail.com>
2024-12-21 20:32:13 +0530

    used sanity typegen generate to generate types in sanity/types.ts file for our typescript project

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,23 +6,7 @@ import { client } from "@/sanity/lib/client";
 export default async function Home({ searchParams }: { searchParams: Promise<{ query: string }> }) {
 
   const query = (await searchParams).query;
-
   const posts = await client.fetch(STARTUPS_QUERY);
-
-  console.log(JSON.stringify(posts, null, 2)); // this null 2 is just for some additional spacing
-
-  // const posts = [
-  //   {
-  //     _createdAt: new Date(),
-  //     views: 55,
-  //     author: { _id: 1, name: 'Harshit' },
-  //     _id: 1,
-  //     description: "This is a description",
-  //     image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F443446%2Fpexels-photo-443446.jpeg%3Fcs%3Dsrgb%26dl%3Ddaylight-forest-glossy-443446.jpg%26fm%3Djpg&f=1&nofb=1&ipt=a6f99529724b7841912b04516a039d699c2c3e3453d1f47279eedbe0b5097fd6&ipo=images',
-  //     category: "Robots",
-  //     title: "We Robots"
-  //   },
-  // ]
 
   return (
     <>
